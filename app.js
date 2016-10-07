e332a45ac5647debdbe3c954ee3b1a03f74a24a5
Sinethemba <dSinethemba@gmail.com>
2016-10-07 10:32:15 +0200

    Updated functioning robbot and moving car

--- app.js
+++ app.js
@@ -3,30 +3,72 @@
 var body = document.querySelector("body");
 
 var taxiPositionCounter = 1;
+var passengers = 7;
 
-body.onkeydown = function(e){
+body.onkeydown = function(e)
+{
+  displayTaxiPassengerCount(passengers);
+  var tl = new TrafficLight(taxiPositionCounter);
 
-    //right arrow : 39
-    //left arrow  : 37
-    //up arrow    : 38
-    //down arrow  : 40
 
+  if(e.keyCode === 39)
+  {
+    if( tl.state() === "red")
+    {
+
+    }
+    else
+    {
+    var currPos = calculateTaxiLocation(taxiPositionCounter);
+    taxiPositionCounter++;
+    //console.log(taxiPositionCounter++);
+    var nextPos = calculateTaxiLocation(taxiPositionCounter);
+    moveTaxi(currPos,nextPos);
+  }
+  }
+
+  if (e.keyCode === 37)
+  {
+    if (tl.state === "red")
+    {
+
+    }
+
+    else
+    {
+    var currPos = calculateTaxiLocation(taxiPositionCounter);
+    taxiPositionCounter--;
+    //console.log(taxiPositionCounter--);
+    var nextPos = calculateTaxiLocation(taxiPositionCounter);
+    moveTaxi(currPos,nextPos);
+    }
+  }
+
+  if(e.keyCode === 38)
+  {
+   if( tl.state() === "green" || tl.state() === "orange")
+   {
+      tl.red();
+
+      displayMessage("The robbots are red, you cannot go!");
+   }
+  }
+
+  if(e.keyCode === 40)
+  {
+  if( tl.state() === "red" || tl.state() === "orange")
+   {
+      tl.green();
+      displayMessage("The robbots are green, you can go!");
+   }
+  }
     displayMessage(e.keyCode);
 
-    // to move the taxi forward...
-    // when the right arrow is pressed
-    //work with `taxiPositionCounter`
-
-    // call `calculateTaxiLocation` to get a taxi location className store it in a variable.
-    // increment the `taxiPositionCounter`
-    // call `calculateTaxiLocation` to the new a taxi location className store it in another variable.
-
-    //pass both to the `moveTaxi( firstClassName, secondClassName)` function
-    //
-
-    var tl = new TrafficLight(2);
-    
+  if (e.keyCode === 13)
+  {
     tl.orange();
+    passengers--;
+  }
 
 
 };
